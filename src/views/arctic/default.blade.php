b58f026785de427d2721f79363ace5f202a8fdc1
laravella <laravella.cms@gmail.com>
2013-10-22 12:34:03 +0200

    point assets to skins package

--- src/views/arctic/default.blade.php
+++ src/views/arctic/default.blade.php
@@ -10,7 +10,7 @@
         <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
     <title>
             @section('title')
-            Laravel Radiate
+            Laravel
             @show
         </title>
         <meta name="description" content="">
---
+++ src/views/common/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.default')
+@include('crud::skins.common.inc.navbar')
+
+{{-- Web site Title --}}
+@section('title')
+@parent
+:: Home
+@stop
+
+@section('content')
+<h1>Laravella</h1>
+@stop
