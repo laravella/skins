f0423e8264abbb6b7ef3f2d4abd91cd2807214f5
laravella <laravella.cms@gmail.com>
2013-10-23 14:40:15 +0200

    move assets into skin folder

--- src/views/arctic/inc/assets.blade.php
+++ src/views/arctic/inc/assets.blade.php
@@ -2,7 +2,7 @@
 <!-- CSS -->
 
 <!-- The stylesheet for the skin -->
-<link rel="stylesheet" href="/packages/laravella/skins/assets/styles/adminstyles.css">
+<link rel="stylesheet" href="/packages/laravella/skins/{{$skin['admin']}}/assets/styles/adminstyles.css">
 
 <!-- The stylesheet for the application that might override that of the skin -->
 <link rel="stylesheet" href="/assets/styles/css/main.css">
@@ -13,19 +13,19 @@
 <!-- JS -->
 
 <!-- jQuery -->
-<script type="text/javascript" src="/packages/laravella/skins/assets/scripts/jquery-1.8.3.min.js"></script>
+<script type="text/javascript" src="/packages/laravella/skins/{{$skin['admin']}}/assets/scripts/jquery-1.8.3.min.js"></script>
 
 <!-- JS -->
-<script type="text/javascript" src="/packages/laravella/skins/assets/scripts/modernizr-2.6.2.min.js"></script>
+<script type="text/javascript" src="/packages/laravella/skins/{{$skin['admin']}}/assets/scripts/modernizr-2.6.2.min.js"></script>
 
 @if(Options::get('debug'))
-<script type="text/javascript" src="/packages/laravella/skins/assets/scripts/jsonconvert.js"></script>
+<script type="text/javascript" src="/packages/laravella/skins/{{$skin['admin']}}/assets/scripts/jsonconvert.js"></script>
 @endif
 
-<script type="text/javascript" src="/packages/laravella/skins/assets/scripts/admintools.js"></script>
+<script type="text/javascript" src="/packages/laravella/skins/{{$skin['admin']}}/assets/scripts/admintools.js"></script>
 
 <!-- CKEditor -->
-<script type="text/javascript" src="/packages/laravella/skins/assets/scripts/ckeditor/ckeditor.js"></script>
+<script type="text/javascript" src="/packages/laravella/skins/{{$skin['admin']}}/assets/scripts/ckeditor/ckeditor.js"></script>
 
 <!-- Internet Explorer .png-fix -->
 
