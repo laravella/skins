8afc0b4b09acb5b2a6103e71a5e57051c75bd927
dataphile <victor@digitalpro.co.za>
2013-10-29 17:50:19 +0200

    add bootswatch skins

---
+++ src/views/slate/inc/assets.blade.php
@@ -0,0 +1,26 @@
+@section('assets_css')
+<!-- CSS -->
+@foreach($assets as $asset)
+    @if($asset['type'] == 'styles')
+    <link rel="stylesheet" href="/packages/laravella/skins/{{$skin['admin']}}/{{$asset['url']}}">
+    @endif
+@endforeach
+@stop
+
+@section('assets_js')		
+<!-- JS -->
+    @foreach($assets as $asset)
+        @if($asset['type'] == 'scripts' & $asset['position'] == 'top')
+        <script type="text/javascript" src="/packages/laravella/skins/{{$skin['admin']}}/{{$asset['url']}}"></script>
+        @endif
+    @endforeach
+@stop
+
+@section('assets_bottom')
+<!-- JS -->
+@foreach($assets as $asset)
+    @if($asset['type'] == 'scripts' & $asset['position'] == 'bottom')
+    <script type="text/javascript" src="/packages/laravella/skins/{{$skin['admin']}}/{{$asset['url']}}"></script>
+    @endif
+@endforeach
+@stop
