c33ed0857b18334bd4cfcff4b052847ee4b93552
laravella <laravella.cms@gmail.com>
2013-11-01 14:24:34 +0200

    add uploader views to skins

---
+++ src/views/flatly/uploader/uploadview.blade.php
@@ -0,0 +1,19 @@
+@section('title')
+File Upload
+@stop
+
+@include('skins::'.$skin['admin'].'.uploader.assets')
+
+@include('skins::'.$skin['admin'].'.uploader.extra_head')
+
+@include('skins::'.$skin['admin'].'.inc.navbar')
+
+@include("skins::".$skin['admin'].".inc.messages")
+
+@include('skins::'.$skin['admin'].'.uploader.getupload')
+
+@include("skins::".$skin['admin'].".inc.bottom")
+
+@section('content')
+    @yield('getUpload')
+@stop
