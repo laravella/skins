01a0731c34d128f5a72fab88218a99992e0bf056
laravella <laravella.cms@gmail.com>
2013-11-01 16:34:17 +0200

    fix menu

--- src/views/flatly/uploader/uploadview.blade.php
+++ src/views/flatly/uploader/uploadview.blade.php
@@ -3,16 +3,12 @@ File Upload
 @stop
 
 @include('skins::'.$skin['admin'].'.uploader.assets')
-
+@include('skins::'.$skin['admin'].'.inc.assets')
 @include('skins::'.$skin['admin'].'.uploader.extra_head')
-
 @include('skins::'.$skin['admin'].'.inc.navbar')
-
-@include("skins::".$skin['admin'].".inc.messages")
-
+@include('skins::'.$skin['admin'].'.inc.messages')
 @include('skins::'.$skin['admin'].'.uploader.getupload')
-
-@include("skins::".$skin['admin'].".inc.bottom")
+@include('skins::'.$skin['admin'].'.inc.bottom')
 
 @section('content')
     @yield('getUpload')
