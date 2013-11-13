0a8d819284360952ada7258b57a424ef5b7afb1f
laravella <laravella.cms@gmail.com>
2013-11-13 11:41:39 +0200

    separate front and back skins

---
+++ src/views/slate/frontview.blade.php
@@ -0,0 +1,14 @@
+@include("skins::common.inc.assets")
+
+@include("skins::common.inc.title")
+@include("skins::common.inc.extra_head")
+
+@include('skins::common.inc.navbar')
+
+@include("skins::common.inc.bottom")
+@include("skins::common.inc.footer")
+@section("skins::common.content")
+
+@yield($action)
+@stop
+
